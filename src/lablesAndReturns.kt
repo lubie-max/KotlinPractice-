680c96d793528cb71acdac22e257abf8ae8555df
lubie-max <shaikhls2421@gmail.com>
2024-06-20 11:50:18 +0530

    lables returns and break

---
+++ src/lablesAndReturns.kt
@@ -0,0 +1,19 @@
+
+
+fun main(){
+    outerLoop@ for (i in 1..10) {
+
+        if( i ==4){
+            break
+        }
+
+        println("i -> $i")
+
+       for (j in 1..7){
+            if (j ==2 )  continue@outerLoop
+        println("j = $j")
+        }
+    }
+
+}
+
