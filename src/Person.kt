2fc2cab6de34d1c13eac63fbb46f7d373d88ee12
lubie-max <shaikhls2421@gmail.com>
2024-06-24 10:38:24 +0530

    day6: classes and constructor

---
+++ src/Person.kt
@@ -0,0 +1,19 @@
+class Person (val name:String , val age:Int) {
+
+    fun action(){
+        println("person walks")
+
+    }
+
+    fun greet(){
+        println("Hi $name and your age is $age")
+    }
+}
+
+
+fun  main(){
+
+    val P = Person ("lubna", 22)
+    P.action()
+    P.greet()
+}
